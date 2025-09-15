c2717c0244b8bd25042896e3a124e4556e8aca73
mhmasum1 <mhmasum231@gmail.com>
2025-09-16 01:15:47 +0600

    state management practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import ToDo from './ToDo.jsx'
 import Actor from './Actor.jsx';
 import Singer from './Singer.jsx';
+import CountShow from './CountShow.jsx';
 function App() {
 
   const actors = ['Bappa ', 'Jasim', 'Kabila', "Sabnur", "Habu", 'Pasha'];
@@ -14,6 +15,8 @@ function App() {
 
   return (
     <>
+      <CountShow></CountShow>
+
       {
         singers.map(singer => <Singer key={singer.id} singer={singer}></Singer>)
       }
---
+++ src/CountShow.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function CountShow() {
+    const [count, setCount] = useState(0);
+    const handleAdd = () => {
+        const newCount = count + 1;
+        setCount(newCount);
+    }
+    const countStyle = {
+        border: '2px solid yellow',
+        padding: '10px'
+    }
+
+    return (
+        <div style={countStyle}>
+            <h3> Count: {count}</h3>
+            <button onClick={handleAdd}>Add</button>
+        </div>
+    )
+}
